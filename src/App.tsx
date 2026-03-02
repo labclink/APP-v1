566e94b1ed0a6075480ea2fc8beb823cdc8c7534
lbca <luis.98bc@gmail.com>
2026-03-02 13:23:33 -0600

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <header className="header">
         <div className="header-content">
           <div className="logo">
-            <span className="logo-text">Innovatech</span>
+            <span className="logo-text">Linko</span>
           </div>
           <nav className="nav">
             <a href="#soluciones">Soluciones</a>
